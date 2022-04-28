8b13d6aa7d15e4e8d02287dab9208d297c4acd64
Aspectys <82753456+Aspectys@users.noreply.github.com>
2022-04-28 22:32:16 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,11 @@
 import java.sql.*;
 import java.util.Scanner;
-public class Main
-{
+public class Main extends Methods {
     private static final String url = "jdbc:mysql://localhost:3306/bankomat?serverTimezone=Europe/Moscow&useSSL=false";
     private static final String username = "root";
     private static final String password = "W4p_Aspect";
 
-    public static void main (String[] args)
+    public static void  main (String[] args)
     {
         Account account = new Account();
         Scanner scanner = new Scanner(System.in);
@@ -28,67 +27,4 @@ public class Main
         }
     }
 
-
-    static class Check {
-        void checkAvailabilityMoney(int moneyOnRequest, int moneyOnAccount){
-            try(Connection conn = DriverManager.getConnection(url, username, password)){
-                if (moneyOnAccount>moneyOnRequest){
-                    System.out.println(".");
-                }else if(moneyOnAccount<moneyOnRequest) {
-                    System.out.printf("checkAvailabilityMoney: На счету не хватает %d $", (moneyOnRequest-moneyOnAccount));
-                }
-            }catch (Exception ex){
-                System.err.println("ERROR");
-                ex.printStackTrace();
-            }
-        }
-
-    }
-    static class Account{
-        void create(String person, int money, String password_){
-            try(Connection conn = DriverManager.getConnection(url, username, password)){
-                PreparedStatement st = conn.prepareStatement("INSERT INTO base (person, money, passwords) VALUES (?, ?, ?)");
-                st.setString(1, person);
-                st.setInt(2, money);
-                st.setString(3, password_);
-                int rs = st.executeUpdate();
-                System.out.printf("%d row(s) returned", rs);
-
-                System.out.println("Account created successful");
-            }catch(Exception ex){
-                System.out.println("Account don`t created(");
-                ex.printStackTrace();
-            }
-        }
-        void transaction(String person1, String person2, int cash){
-
-            try (Connection conn = DriverManager.getConnection(url, username, password)){
-                PreparedStatement person_first = conn.prepareStatement("SELECT id, person, passwords, money FROM base WHERE person = ?;"); person_first.setString(1, person1);
-                PreparedStatement person_second = conn.prepareStatement("SELECT id, person, passwords, money FROM base WHERE person = ?;");person_second.setString(1, person2);
-                ResultSet resultSet1 = person_first.executeQuery();
-
-                while(resultSet1.next()){
-                    int money1 = resultSet1.getInt("money"); // Kolvo money u Alimzhan
-
-                    Check check = new Check();
-                    check.checkAvailabilityMoney(cash, money1);
-                    int money2 = 0;
-                    money2 = money1 + cash; money1 = money1 - cash;
-
-                    PreparedStatement m = conn.prepareStatement("update base set money = ? where person = ?;");
-                            m.setInt(1, money1); m.setString(2, person1);
-                            int mRun = m.executeUpdate();
-                            System.out.println(mRun);
-
-                    PreparedStatement p = conn.prepareStatement("update base set money = ? where person = ?;");
-                            p.setInt(1, money2); p.setString(2, person2);
-                            int pRun = p.executeUpdate();
-                            System.out.println(pRun);
-                }
-            }catch (Exception ex){
-                System.out.println("ERROR");
-                ex.printStackTrace();
-            }
-        }
-    }
 }
